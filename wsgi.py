dad771e301d232dfc44c01e7ddb695ff1d5ef5ae
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2023-08-09 16:17:34 -0700

    added proxy fix

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask import render_template
 from src.nest_message.nested_bitmap import to_bit_msg
 import re
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 
 @app.route("/")
