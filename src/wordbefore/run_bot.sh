80a39970c8f625b500dc4dc8b1f6ed3dc1737597
yoni <jonathan.d.ackerman@gmail.com>
2023-12-01 19:57:18 -0800

    added process reporting to run_bot.sh

--- src/wordbefore/run_bot.sh
+++ src/wordbefore/run_bot.sh
@@ -1,8 +1,15 @@
 
+DATE=/usr/bin/date
+
 cd ~/andor
 source venv/bin/activate
 cd ./src/wordbefore/
 python3 get_wordles.py
+if [[ $? -eq 0 ]]; then 
+    echo "wordles updated on $DATE"
+else
+    echo "wordles update failed on $DATE"
+fi
 deactivate
 
 
