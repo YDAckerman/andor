68079480b2a8bf042f01eb7dfb41659f287ba79f
yoni <jonathan.d.ackerman@gmail.com>
2023-12-03 09:48:42 -0800

    cron still not working

--- src/wordbefore/run_bot.sh
+++ src/wordbefore/run_bot.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 DATE=$(/usr/bin/date)
 
-cd ~/andor/src/wordbefore/
-
-~/andor/venv/bin/python3 get_wordles.py
+~/andor/venv/bin/python3 ~/andor/src/wordbefore/get_wordles.py
 
 if [[ $? -eq 0 ]]; then 
     echo "wordles updated on $DATE"
 else
     echo "wordles update failed on $DATE"
 fi
-
-cd
 
